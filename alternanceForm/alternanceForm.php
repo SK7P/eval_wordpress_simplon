2ac47cbadf392fd328c4a6804b4f527f45cb8a5f
SK7P <laup.erlanjimmy@gmail.com>
2023-09-19 16:01:17 +0200

    Ajout des dernières fonctionnalités et style

--- alternanceForm/alternanceForm.php
+++ alternanceForm/alternanceForm.php
@@ -34,7 +34,7 @@ function alternance_form_install()
 		created_at datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
 		firstname tinytext NOT NULL,
     lastname tinytext NOT NULL,
-    birth_date tinytext NOT NULL,
+    birth_date datetime NOT NULL,
     degree tinytext NOT NULL,
     current_study_lvl tinytext NOT NULL,
     formation_goal text NOT NULL,
@@ -51,7 +51,6 @@ function alternance_form_install()
 function add_registration()
 {
   ob_start();
-  var_dump($_POST);
   global $wpdb;
 
   $created_at = current_time('mysql');
@@ -63,12 +62,21 @@ function add_registration()
   $formation_goal = $_POST['formation_goal'];
   $city = $_POST['city'];
   $mobility = $_POST['mobility'];
-
+  $free_notes = $_POST['free_notes'];
   $table_name = $wpdb->prefix . 'registrations';
-
-  $data_insert = $wpdb->insert(
-    $table_name,
-    array(
+  $data = array(
+    'created_at' => $created_at,
+    'firstname' => $firstname,
+    'lastname' => $lastname,
+    'birth_date' => $birth_date,
+    'degree' => $degree,
+    'current_study_lvl' => $current_study_lvl,
+    'formation_goal' => $formation_goal,
+    'city' => $city,
+    'mobility' => $mobility
+  );
+  if ($free_notes) {
+    $data = array(
       'created_at' => $created_at,
       'firstname' => $firstname,
       'lastname' => $lastname,
@@ -78,7 +86,13 @@ function add_registration()
       'formation_goal' => $formation_goal,
       'city' => $city,
       'mobility' => $mobility,
-    )
+      'free_notes' => $free_notes
+    );
+  }
+
+  $data_insert = $wpdb->insert(
+    $table_name,
+    $data
   );
   if ($data_insert) {
     echo '<h2> Votre inscription a bien été enregistrée ! Merci :) </h2>';
@@ -93,39 +107,46 @@ function form_func()
 ?>
 
   <form action="http://localhost:8080/?page_id=21" method="post">
-    <div>
-      <label for="firstname">firstname:</label>
+    <div class="input-container padding-top">
+      <label for="firstname">Prénom:</label>
       <input type="text" id="firstname" name="firstname" required />
     </div>
-    <div>
-      <label for="lastname">lastname:</label>
+    <div class="input-container padding-top">
+      <label for="lastname">Nom:</label>
       <input type="text" id="lastname" name="lastname" required />
     </div>
-    <div>
-      <label for="birth_date">birth_date:</label>
-      <input type="text" id="birth_date" name="birth_date" required />
+    <div class="input-container padding-top">
+      <label for="birth_date">Date de naissance:</label>
+      <input type="date" id="birth_date" name="birth_date" required />
     </div>
-    <div>
-      <label for="degree">degree:</label>
+    <div class="input-container padding-top">
+      <label for="degree">Libellé du diplôme actuel:</label>
       <input type="text" id="degree" name="degree" required />
     </div>
-    <div>
-      <label for="current_study_lvl">current_study_lvl:</label>
+    <div class="input-container padding-top">
+      <label for="current_study_lvl">Niveau d'études actuel:</label>
       <input type="text" id="current_study_lvl" name="current_study_lvl" required />
     </div>
-    <div>
-      <label for="formation_goal">formation_goal:</label>
+    <div class="input-container padding-top">
+      <label for="formation_goal">Formation visée:</label>
       <input type="text" id="formation_goal" name="formation_goal" required />
     </div>
-    <div>
-      <label for="city">city:</label>
+    <div class="input-container padding-top">
+      <label for="city">Ville d'habitation:</label>
       <input type="text" id="city" name="city" required />
     </div>
-    <div>
-      <label for="mobility">mobility:</label>
+    <div class="input-container padding-top">
+      <label for="mobility">Mobilité (en kilomètres depuis le lieu de vie):</label>
       <input type="number" id="mobility" name="mobility" required />
     </div>
-    <button type="submit">Submit</button>
+    <div class="input-container padding-top">
+      <label for="free_notes">Notes:</label>
+      <textarea type="text" id="free_notes" name="free_notes">
+      </textarea>
+    </div>
+    <div class="input-container padding-top">
+      <input type="submit" value="Continuer" />
+    </div>
   </form>
   <?php
 
@@ -152,7 +173,6 @@ function get_registrations()
         echo '<th>' . $col . '</th>';
       }
       foreach ($registrations as $registration) {
-        print_r($registration);
         echo
         '<tr>' .
           '<td>' . $registration->id . '</td>' .
@@ -176,6 +196,55 @@ function get_registrations()
   }
   return ob_get_clean();
 }
+
+function add_custom_menu()
+{
+  add_menu_page(
+    'Gérer les Inscriptions',
+    'Gérer les Inscriptions',
+    'manage_options',
+    'manage-registrations',
+    'display_registration_list'
+  );
+}
+
+add_action('admin_menu', 'add_custom_menu');
+
+function display_registration_list()
+{
+  global $wpdb;
+
+  $table_name = $wpdb->prefix . 'registrations';
+  $registrations = $wpdb->get_results("SELECT * FROM $table_name");
+
+  echo '<div class="wrap">';
+  echo '<h2>Liste des Inscriptions</h2>';
+  echo '<table class="widefat">';
+  echo '<thead><tr>';
+  echo '<th>ID</th>';
+  echo '<th>CreatedAt</th>';
+  echo '<th>Nom Prénom</th>';
+  echo '<th>Date de Naissance</th>';
+  echo "<th>Niveau d'études actuel</th>";
+  echo '<th>Formation visée</th>';
+  echo '</tr></thead>';
+  echo '<tbody>';
+  foreach ($registrations as $registration) {
+    echo '<tr>';
+    echo '<td>' . $registration->id . '</td>';
+    echo '<td>' . $registration->created_at . '</td>';
+    echo '<td>' . $registration->firstname . ' ' . $registration->lastname . '</td>';
+    echo '<td>' . $registration->birth_date . '</td>';
+    echo '<td>' . $registration->current_study_lvl . '</td>';
+    echo '<td>' . $registration->formation_goal . '</td>';
+    echo '</tr>';
+  }
+  echo '</tbody>';
+  echo '</table>';
+  echo '</div>';
+}
+
+
 
 add_shortcode('form', 'form_func');
 add_shortcode('get_registrations', 'get_registrations');
